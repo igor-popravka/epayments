f2b8448326d5c3b5bfa9beb52eecf9d7ff83907b
Igor Popravka <igor.popravka@tstechpro.com>
2020-02-24 10:42:47 +0200

    prepare base template

--- application/classes/Controller/Base.php
+++ application/classes/Controller/Base.php
@@ -7,7 +7,18 @@ abstract class Controller_Base extends Controller_Template {
     {
         parent::before();
 
-        $this->template->styles = [];
-        $this->template->scripts = [];
+        $this->template->title = 'Test';
+        $this->template->styles = [
+            HTML::style('media/css/bootstrap.min.css'),
+            HTML::style('media/css/style.css'),
+        ];
+        $this->template->scripts = [
+            HTML::script('media/js/jquery-3.4.1.slim.min.js'),
+            HTML::script('media/js/bootstrap.bundle.min.js')
+        ];
+        $this->template->navbar = View::factory('navbar');
+        $this->template->jumbotron = View::factory('jumbotron');
+        $this->template->container = View::factory('container');
+        $this->template->footer = View::factory('footer');
     }
 }
---
+++ application/views/footer.php
@@ -0,0 +1,1 @@
+<p>&copy; Company 2017-2019</p>
